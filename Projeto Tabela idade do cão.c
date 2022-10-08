534d617093c0411fa8c47bfc53256a703e4069a9
Leandro Costa <leandro.costarj377@gmail.com>
2022-10-08 20:19:03 -0300

    Novo modelo

---
+++ Projeto Tabela idade do cão.c
@@ -0,0 +1,170 @@
+#include<stdio.h>
+
+int main()
+{
+      int idade;
+      int tamanho;
+    
+    printf("\n  ## Descubra a Idade do seu Cachorro ##\n    ## Na idade dos Seres humanos ## ");
+    printf("\n\n    #  Qual a Idade do seu cachorro? #\n                  ");
+     scanf("%i", &idade);
+    printf("\n # Qual o Tamanho do Animal? #\n (1) pequeno:\n (2) médio:\n (3) grande:     ");
+     scanf("%i", &tamanho);
+  
+  
+   if ( idade == 1){
+    printf("\n  # Idade Humana do seu Animal 15 anos #  ");
+  }
+
+   if ( idade == 2){
+    printf("\n  # Idade Humana do seu Animal 24 anos #  ");
+  }
+   
+   if ( idade == 3){
+    printf("\n  # Idade Humana do seu Animal 28 anos #  ");
+  }
+
+   if ( idade == 4 ){
+    printf("\n  # Idade Humana do seu Animal 32 anos #  ");
+  }
+
+   if ( idade == 5){
+    printf("\n  # Idade Humana do seu Animal 36 anos #  ");
+  }
+  
+   if ( idade == 6 && tamanho == 1){
+    printf("\n  # Idade Humana do seu Animal 40 anos #  ");
+  }
+
+   if ( idade == 6 && tamanho == 2){
+    printf("\n  # Idade Humana do seu Animal 42 anos #  ");
+  }
+
+   if ( idade == 6 && tamanho == 3){
+    printf("\n  # Idade Humana do seu Animal 45 anos #  ");
+  }
+
+   if ( idade == 7 && tamanho == 1){
+    printf("\n  # Idade Humana do seu Animal 44 anos #  ");
+  }
+
+   if ( idade == 7 && tamanho == 2){
+    printf("\n  # Idade Humana do seu Animal 47 anos #  ");
+  }
+
+   if ( idade == 7 && tamanho == 3){
+    printf("\n  # Idade Humana do seu Animal 50 anos #  ");
+  }
+
+   if ( idade == 8 && tamanho == 1){
+    printf("\n  # Idade Humana do seu Animal 48 anos #  ");
+  }
+
+   if ( idade == 8 && tamanho == 2){
+    printf("\n  # Idade Humana do seu Animal 51 anos #  ");
+  }
+
+   if ( idade == 8 && tamanho == 3){
+    printf("\n # Idade Humana do seu Animal 55 anos #   ");
+  }
+
+   if ( idade == 9 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 52 anos #   ");
+  }
+
+   if ( idade == 9 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 56 anos #   ");
+  }
+
+   if ( idade == 9 && tamanho == 3){
+    printf("\n # Idade Humana do");
+  }
+
+   if ( idade == 10 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 56 anos #   ");
+  }
+
+   if ( idade == 10 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 60 anos #   ");
+  }
+
+   if ( idade == 10 && tamanho == 3){
+    printf("\n # Idade Humana do seu Animal 66 anos #   ");
+  }
+
+   if ( idade == 11 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 60 anos #   ");
+  }
+
+   if ( idade == 11 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 65 anos #   ");
+  }
+
+   if ( idade == 11 && tamanho == 3){
+    printf("\n # Idade Humana do seu Animal 72 anos #   ");
+  }
+
+   if ( idade == 12 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 64 anos #    ");
+  }
+
+   if ( idade == 12 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 69 anos #    ");
+  }
+
+   if ( idade == 12 && tamanho == 3){
+    printf("\n # Idade Humana do seu Animal 77 anos #    ");
+  }
+
+   if ( idade == 13 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 68 anos #    ");
+  }
+
+   if ( idade == 13 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 74 anos #    ");
+  }
+
+   if ( idade == 13 && tamanho == 3){ 
+    printf("\n # Idade Humana do seu Animal 82 anos      ");
+  }
+
+   if ( idade == 14 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 72 anos #    ");
+  }
+
+   if ( idade == 14 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 78 anos #    ");
+  }
+
+   if ( idade == 14 && tamanho == 3){
+    printf("\n # Idade Humana do seu Animal 88 anos #    ");
+  }
+
+   if ( idade == 15 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 76 anos #    ");
+  }
+
+   if ( idade == 15 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 83 anos #    ");
+  }
+
+   if ( idade == 15 && tamanho == 3){
+    printf("\n # Idade Humana do seu Animal 93 anos #    ");
+  }
+
+   if ( idade == 16 && tamanho == 1){
+    printf("\n # Idade Humana do seu Animal 80 anos #    ");
+  }
+  
+   if ( idade == 16 && tamanho == 2){
+    printf("\n # Idade Humana do seu Animal 87 anos #    ");
+  } 
+
+   if ( idade == 16 && tamanho == 3){
+    printf("\n # Idade Humana do seu Animal 120 anos #   ");
+  }
+
+
+
+
+}
